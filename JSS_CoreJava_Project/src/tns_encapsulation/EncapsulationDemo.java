591525de9969060063cb77f2a74a61fe9b0bd417
Manjula16-code <manjulavanik16@gmail.com>
2025-07-30 20:55:07 +0530

    first commit

---
+++ JSS_CoreJava_Project/src/tns_encapsulation/EncapsulationDemo.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package tns_encapsulation;
+
+/**
+ * 
+ */
+public class EncapsulationDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OopsDemoCtc obj = new OopsDemoCtc();
+		obj.setAge(21);
+		obj.setName("Manjula");
+		obj.setEmail("vani@gmail.com");
+System.out.println(obj);
+
+	}
+
+}
---
+++ JSS_CoreJava_Project/src/tns_encapsulation/PersonDemo.java
@@ -0,0 +1,15 @@
+package tns_encapsulation;
+
+public class PersonDemo {
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+			Person p = new Person();
+			p.setName("Jagan");
+			p.setGender("Male");
+			p.setAge(18);
+			p.setIncome(50000);
+			
+	        System.out.println(p);
+		}
+}
+
