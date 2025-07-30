b87f9f4039bfeb08036649fa60058632a57be38f
ManjulaGit25 <manjulavani.neet2021@gmail.com>
2025-07-30 20:14:47 +0530

    first commit

---
+++ JSS_CoreJava_Project/src/com/tnsif/samples/Firstprog.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.tnsif.samples;
+
+/**
+ * 
+ */
+public class Firstprog {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I Did it !!");
+
+	}
+
+}
